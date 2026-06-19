e47c01c3a3b1de5c5141a6da4cc433bbe91e542a
cqb13 <cqb13.main@gmail.com>
2026-06-19 13:40:23 -0400

    added clue preview

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module github/cqb13/xword-preview
 
 go 1.25.1
 
-require github.com/gen2brain/raylib-go/raylib v0.55.1
+require github.com/gen2brain/raylib-go/raylib v0.60.0
 
 require (
 	github.com/cqb13/puz-parser v0.0.0-20260225163210-47dba40bde0a // indirect
-	github.com/ebitengine/purego v0.9.1 // indirect
-	golang.org/x/exp v0.0.0-20251209150349-8475f28825e9 // indirect
+	github.com/ebitengine/purego v0.10.1 // indirect
+	github.com/gen2brain/raylib-go/raygui v0.0.0-20260601164102-8d36eb1fc4fb // indirect
+	github.com/jupiterrider/ffi v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20260611194520-c48552f49976 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,103 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/cqb13/puz-parser/puz"
+	rlgui "github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+const PADDING = 10
+const FONT_SIZE = 20
+
 func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("Not enough args. xword-preview [crossword]")
+	}
+
+	bytes, err := os.ReadFile(args[0])
+	if err != nil {
+		fmt.Println("Failed to load crossword: ", err)
+		os.Exit(1)
+	}
+
+	puzzle, err := puz.DecodePuz(bytes)
+	if err != nil {
+		fmt.Println("Failed to decode crossword: ", err)
+		os.Exit(1)
+	}
+
+	rl.SetConfigFlags(rl.FlagWindowResizable)
+	rl.InitWindow(600, 600, fmt.Sprintf("%s | %s", puzzle.Title, puzzle.Author))
+	defer rl.CloseWindow()
+
+	halfHeight := float32(rl.GetScreenHeight() / 2)
+	thirdWidth := float32(rl.GetScreenWidth() / 3)
+
+	var (
+		downCluesPanelRec        = rl.NewRectangle(PADDING, PADDING, thirdWidth, halfHeight)
+		downCluesPanelContentRec = rl.NewRectangle(PADDING, PADDING, float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight()))
+		downCluesPanelView       = rl.NewRectangle(0, 0, 0, 0)
+		downViewPanelScroll      = rl.NewVector2(99, 0)
+
+		acrossCluesPanelRec        = rl.NewRectangle(PADDING, PADDING, thirdWidth, halfHeight)
+		acrossCluesPanelContentRec = rl.NewRectangle(PADDING, PADDING, float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight()))
+		acrossCluesPanelView       = rl.NewRectangle(0, 0, 0, 0)
+		acrossViewPanelScroll      = rl.NewVector2(99, 0)
+	)
+
+	for !rl.WindowShouldClose() {
+		halfHeight = float32(rl.GetScreenHeight() / 2)
+		thirdWidth := float32(rl.GetScreenWidth() / 3)
+
+		downCluesPanelRec = rl.NewRectangle(PADDING, PADDING, thirdWidth, halfHeight-PADDING*2)
+		downCluesPanelContentRec = rl.NewRectangle(PADDING, PADDING, float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight()))
+
+		acrossCluesPanelRec = rl.NewRectangle(PADDING, halfHeight, thirdWidth, halfHeight-PADDING)
+		acrossCluesPanelContentRec = rl.NewRectangle(PADDING, PADDING, float32(rl.GetScreenWidth()), float32(rl.GetScreenHeight()))
+
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		// Down Clues
+		rlgui.ScrollPanel(downCluesPanelRec, "Down", downCluesPanelContentRec, &downViewPanelScroll, &downCluesPanelView)
+		rl.BeginScissorMode(int32(downCluesPanelView.X), int32(downCluesPanelView.Y), int32(downCluesPanelView.Width), int32(downCluesPanelView.Height))
+
+		height := int32(0)
+		for _, clue := range puzzle.GetCluesByDirection(puz.Down) {
+			text := fmt.Sprintf("%d. %s", clue.Num, clue.Clue)
+
+			width := rl.MeasureText(text, FONT_SIZE)
+
+			_ = width
+
+			rl.DrawText(text, int32(downCluesPanelView.X+downViewPanelScroll.X), int32(downCluesPanelView.Y+downViewPanelScroll.Y)+height, FONT_SIZE, rl.Black)
+			height += FONT_SIZE
+		}
+
+		rl.EndScissorMode()
+
+		// Across Clues
+		rlgui.ScrollPanel(acrossCluesPanelRec, "Across", acrossCluesPanelContentRec, &acrossViewPanelScroll, &acrossCluesPanelView)
+		rl.BeginScissorMode(int32(acrossCluesPanelView.X), int32(acrossCluesPanelView.Y), int32(acrossCluesPanelView.Width), int32(acrossCluesPanelView.Height))
+
+		height = int32(0)
+		for _, clue := range puzzle.GetCluesByDirection(puz.Across) {
+			text := fmt.Sprintf("%d. %s", clue.Num, clue.Clue)
+
+			width := rl.MeasureText(text, FONT_SIZE)
+
+			_ = width
+
+			rl.DrawText(text, int32(acrossCluesPanelView.X+acrossViewPanelScroll.X), int32(acrossCluesPanelView.Y+acrossViewPanelScroll.Y)+height, FONT_SIZE, rl.Black)
+			height += FONT_SIZE
+		}
+
+		rl.EndScissorMode()
+
+		rl.EndDrawing()
+	}
 }
